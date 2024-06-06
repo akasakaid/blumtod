ec907af8414773f094f5cc2a699a2ad768ef36e5
Fawwaz Thoerif <fawwazthoerif@gmail.com>
2024-06-06 22:46:35 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import json
+import random
 import requests
 from json import dumps as dp, loads as ld
 from datetime import datetime
@@ -93,13 +95,15 @@ class BlumTod:
         self.log(f"{hijau}balance after claim : {balance}")
         return
 
-    def get_balance(self, access_token):
+    def get_balance(self, access_token,only_show_balance=False):
         url = "https://game-domain.blum.codes/api/v1/user/balance"
         headers = self.base_headers.copy()
         headers["Authorization"] = f"Bearer {access_token}"
         res = self.http(url, headers)
         balance = res.json()["availableBalance"]
         self.log(f"{hijau}balance : {putih}{balance}")
+        if only_show_balance:
+            return
         timestamp = round(res.json()["timestamp"] / 1000)
         if "farming" not in res.json().keys():
             return False, "not_started"
@@ -144,6 +148,37 @@ class BlumTod:
                 return
             self.log(f"{merah}failed claim referral bonus !")
             return
+    
+    def playgame(self,access_token):
+        url_play = "https://game-domain.blum.codes/api/v1/game/play"
+        url_claim = "https://game-domain.blum.codes/api/v1/game/claim"
+        url_balance = "https://game-domain.blum.codes/api/v1/user/balance"
+        config = json.loads(open('config.json','r').read())
+        game_cfg = config['game_point']
+        low = game_cfg['low']
+        high = game_cfg['high']
+        if low > high:
+            self.log(f'{kuning}high value must be higher than lower value')
+            return
+        headers = self.base_headers.copy()
+        headers['Authorization'] = f"Bearer {access_token}"
+        res = self.http(url_balance,headers)
+        play = res.json()['playPasses']
+        self.log(f'{hijau}you have {putih}{play}{hijau} game ticket')
+        for i in range(play):
+            res = self.http(url_play,headers,'')
+            game_id = res.json()['gameId']
+            self.countdown(30)
+            point = random.randint(low,high)
+            data = json.dumps({"gameId":game_id,"points":point})
+            res = self.http(url_claim,headers,data)
+            if "OK" in res.text:
+                self.log(f'{hijau}success earn {putih}{point}{hijau} from game !')
+                self.get_balance(access_token,only_show_balance=True)
+                continue
+            
+            self.log(f'{merah}failed earn {putih}{point}{merah} from game !')
+            continue
 
     def data_parsing(self, data):
         redata = {}
@@ -166,6 +201,10 @@ class BlumTod:
                     open(".http_request.log", "a", encoding="utf-8").write(
                         res.text + "\n"
                     )
+                    if "<html>" in res.text:
+                        time.sleep(2)
+                        continue
+                    
                     return res
 
                 if data == "":
@@ -173,10 +212,18 @@ class BlumTod:
                     open(".http_request.log", "a", encoding="utf-8").write(
                         res.text + "\n"
                     )
+                    if "<html>" in res.text:
+                        time.sleep(2)
+                        continue
+                    
                     return res
 
                 res = requests.post(url, headers=headers, data=data, timeout=30)
                 open(".http_request.log", "a", encoding="utf-8").write(res.text + "\n")
+                if "<html>" in res.text:
+                    time.sleep(2)
+                    continue
+                
                 return res
 
             except (
@@ -221,7 +268,10 @@ class BlumTod:
         while True:
             list_countdown = []
             for no,data in enumerate(datas):
-                self.log(f'{hijau}account number - {putih}{no}')
+                self.log(f"{hijau}account number - {putih}{no + 1}")
+                data_parse = self.data_parsing(data)
+                user = json.loads(data_parse['user'])
+                self.log(f"{hijau}login as : {putih}{user['first_name']}")
                 access_token = self.renew_access_token(data)
                 self.get_friend(access_token)
                 self.solve_task(access_token)
@@ -232,6 +282,7 @@ class BlumTod:
                 if isinstance(res_bal, str):
                     res_bal = self.start_farming(access_token)
                 list_countdown.append(res_bal)
+                self.playgame(access_token)
                 self.log(self.garis)
             min_countdown = min(list_countdown)
             now = int(time.time())
